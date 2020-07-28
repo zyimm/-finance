c0fc1a64653130728f0114e1cffd95f560c52c3a
zyimm <799783009@qq.com>
2020-07-28 10:04:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-//zyimm-hxcf~2525
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 定义应用目录
@@ -8,4 +7,3 @@ define('ROOT_PATH',dirname(__FILE__));
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',1);
 require './ThinkPHP/ThinkPHP.php';
-echo time();
